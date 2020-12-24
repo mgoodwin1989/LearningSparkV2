18bedf4905f2a5e8b3c23026e6e19dd48830f793
Matt Goodwin <mgoodwin1989@gmail.com>
2020-12-23 19:12:45 -0500

    Added java examples

---
+++ chapter3/java/src/main/java/org/example/chapter3/App.java
@@ -0,0 +1,41 @@
+package org.example.chapter3;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructType;
+
+
+
+import java.util.Arrays;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        SparkSession sparkSession = SparkSession.builder()
+                                    .master("local")
+                                    .appName("schemabuilder")
+                                    .getOrCreate();
+        String filename = args[0];
+        StructType schema = DataTypes.createStructType(Arrays.asList(
+                    DataTypes.createStructField("Id", DataTypes.IntegerType, false),
+                    DataTypes.createStructField("First", DataTypes.StringType, false),
+                    DataTypes.createStructField("Last", DataTypes.StringType, false),
+                    DataTypes.createStructField("Url", DataTypes.StringType, false),
+                    DataTypes.createStructField("Published", DataTypes.StringType, false),
+                    DataTypes.createStructField("Hits", DataTypes.IntegerType, false),
+                    DataTypes.createStructField("Campaigns", DataTypes.createArrayType(DataTypes.StringType, false), false)));
+        
+        Dataset<Row> ds = sparkSession.read().schema(schema).json(filename);
+        ds.show(false);
+        ds.printSchema();
+        System.out.println(ds.schema());
+        sparkSession.stop();
+    }
+}
